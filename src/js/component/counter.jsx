64b70cde07d1201f01be338f756962f841ff4b75
Wendy0206 <155206385+Wendy0206@users.noreply.github.com>
2024-02-27 17:18:58 +0000

    96% done

--- src/js/component/counter.jsx
+++ src/js/component/counter.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import { useState, useEffect } from 'react';
 
 
+
 //create your first component
 export const TimeCount= ()=> {
 
-    const [counter, setCounter] = useState(57);
-    const [counterM, setCounterM] = useState(58);
+    const [counter, setCounter] = useState(0);
+    const [counterM, setCounterM] = useState(0);
     const [counterH, setCounterH] = useState(0);
 
 let  interval_sec;
@@ -14,13 +15,12 @@ let  interval_sec;
   useEffect(() => {
      interval_sec = setInterval(() => {
       setCounter(counter+1);
-     // setCounterM(Math.trunc(counter/60))
       if(counter==59){
         if(counterM==59){
           setCounterM(0);
           setCounter(0);
           setCounterH(counterH+1);
-          console.log('test')
+         
         }
         else{
         setCounter(0);
@@ -69,6 +69,7 @@ function startCounter (){
 
     }
     
+    
 
 	return	(
 
@@ -86,15 +87,18 @@ function startCounter (){
             {counterM} </span> 
         <span class="txt">Sec  </span> 
         <span class="digit" id="count"> 
-            {counter}  </span> 
+            {counter} </span> 
     </div> 
     <div id="buttons"> 
         <button class="btn" id="start" onClick={()=>startCounter()}> 
-            Start</button> 
+        <i class="fas fa-play"></i></button> 
         <button class="btn" id="stop" onClick={()=>stopCounter()}> 
-            Stop</button> 
+        <i class="fa-solid fa-stop"></i></button> 
         <button class="btn" id="reset" onClick={()=>resetCounter()}> 
-            Reset</button> 
+        <i class="fa-solid fa-repeat"></i></button> 
+            <button class="btn btn-dark"  onClick={()=>resetCounter()}> 
+            R</button> 
+         
     </div> 
 </div> 
 
